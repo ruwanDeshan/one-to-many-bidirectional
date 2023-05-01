3a068586bf93618cfd82e913545211978fda2868
ruwanDeshan <117300634+ruwanDeshan@users.noreply.github.com>
2023-05-01 13:28:26 +0530

    initilize

---
+++ src/entity/Student.java
@@ -0,0 +1,56 @@
+package entity;
+
+import java.util.List;
+
+@Entity(name = "student_table")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private long studentId;
+    private String studentName;
+
+    //------------mapping-----------
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "student_laptop_details",
+            joinColumns = @JoinColumn(name = "stu_id"),
+            inverseJoinColumns = @JoinColumn(name = "lap_id")
+    )
+    private List<Laptop> laptops;
+
+    //------------mapping-----------
+
+
+    public Student() {
+    }
+
+    public Student(long studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public List<Laptop> getLaptops() {
+        return laptops;
+    }
+
+    public void setLaptops(List<Laptop> laptops) {
+        this.laptops = laptops;
+    }
+}
